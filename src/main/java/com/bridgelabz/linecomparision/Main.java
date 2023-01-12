fe92facdb337826305301263a5425969e0bbdc5a
Akshatha <49878763+Akshu1@users.noreply.github.com>
2023-01-12 16:23:40 +0530

    Update Main.java

--- src/main/java/com/bridgelabz/linecomparision/Main.java
+++ src/main/java/com/bridgelabz/linecomparision/Main.java
@@ -2,6 +2,6 @@ package com.bridgelabz.linecomparision;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Line Comparison!");
     }
 }
